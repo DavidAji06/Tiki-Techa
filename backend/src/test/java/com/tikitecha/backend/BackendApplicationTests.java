21ffe321aa70c387afa23f70ab66ee48cefecc56
DavidAji06 <davidaji309@gmail.com>
2026-08-04 13:13:10 +0100

    Init commit -> project skeleton + docker setup

---
+++ backend/src/test/java/com/tikitecha/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tikitecha.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
